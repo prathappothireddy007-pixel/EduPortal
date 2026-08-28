7df93e3a80bca2418c72f15884e178b2a087c5d2
Prathap Pothireddy <prathappothireddy007@gmail.com>
2026-08-28 18:15:04 +0530

    feat: integrate KageLandingPage exact source and assets from ThreeUI

---
+++ src/Scene.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { KageLandingPage } from "./shaders/landing-pages/LandingPages";
+import "./shaders/threeui.css";
+
+export function Scene() {
+  return (
+    <div className="shader-frame">
+      <KageLandingPage
+        headingFont="onest"
+        bodyFont="onest"
+        headingWeight="400"
+        bodyWeight="300"
+        primaryColor="#e0231c"
+        headingSize={46}
+        bodySize={17}
+        headingLetterSpacing={-0.012}
+      />
+    </div>
+  );
+}
+
+export default Scene;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./shaders/landing-pages/LandingPages";
+export * from "./shaders/landing-pages/pageTypography";
+export * from "./shaders/landing-pages/pageRecipes";
+export { Scene } from "./Scene";
